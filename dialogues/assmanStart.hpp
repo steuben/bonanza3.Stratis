59920d845cc7212c8f83a7e90388b3c8f8608078
steuben <ckluge@gmail.com>
2013-03-15 21:54:12 +0100

    ADDED: Dialogues (in development)

---
+++ dialogues/assmanStart.hpp
@@ -0,0 +1,36 @@
+
+
+#include "assmanCommons.hpp"
+
+// HEADLINE
+ class titleHome : titleBarText
+  {
+	text = " BLUFOR ASSMAN";
+  };
+
+  
+// SUBLINES
+  class subtitleH1_support_artillery : subtitleH1
+  {
+	text = "ARTILLERY SUPPORT";
+	x = 0.415; y = 0.28;
+	
+  };
+  
+  class subtitleH1_support_air : subtitleH1
+  {
+	text = "AIR SUPPORT";
+	x = 0.71; y = 0.28;
+	
+  };
+  
+  class subtitleH1_support_satscan : subtitleH1
+  {
+	text = "ADVANCED ASSETS";
+	x = 0.415; y = 0.4435;
+	
+  };
+
+  
+  
+ 
---
+++ dialogues/ui.hpp
@@ -0,0 +1,11 @@
+class uiAssmanStart 
+{
+
+  idd = -1;
+  movingEnable = true;
+  controlsBackground[] = {"bgShape","titleHome"};
+  controls[] = {"subtitleH1_support_artillery","subtitleH1_support_air","subtitleH1_support_satscan", "btn_close"};
+  objects[] = {};
+  
+  #include "assmanStart.hpp"
+};
