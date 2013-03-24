4b13e6898ccd2ce15e6b819ba0e6d1a15e4baee9
steuben <ckluge@gmail.com>
2013-03-24 22:41:14 +0100

    CHANGED: Dialogues cleanup, ASSMAN outsourced

--- dialogues/ui.hpp
+++ dialogues/ui.hpp
@@ -1,37 +1,6 @@
-class uiAssmanStart 
-{
-
-  idd = -1;
-  movingEnable = true;
-  controlsBackground[] = {"bgShape","titleHome"};
-  controls[] = {"btn_reqFiremission","btn_reqFixedwing","btn_reqRotarywing","btn_reqCCP","btn_reqINFIL","btn_reqEXFIL","btn_close"};
-  objects[] = {};
-  
-  #include "assmanStart.hpp"
-};
 
 
-class uiAssmanDismissArty 
-{
+// ASSMAN
+	#include "assman\assmanViews.hpp"
 
-  idd = -1;
-  movingEnable = true;
-  controlsBackground[] = {"bgShapeDismiss","titleBarText"};
-  controls[] = {"titleDismissArty","btn_backToStart","btn_dismissArty","btn_close"};
-  objects[] = {};
-  
-  #include "assmanDismissArty.hpp"
-};
-
-class uiAssmanDismissRotarywing 
-{
-
-  idd = -1;
-  movingEnable = true;
-  controlsBackground[] = {"bgShapeDismiss","titleBarText"};
-  controls[] = {"titleDismissRotarywing","btn_backToStart","btn_dismissRotarywing","btn_close"};
-  objects[] = {};
-  
-  #include "assmanDismissRotarywing.hpp"
-};
 
