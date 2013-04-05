e5b4428b568716415bc2515dbac9ee6b54ebc58a
steuben <ckluge@gmail.com>
2013-04-05 23:43:38 +0200

    WIP commit

---
+++ dialogues/assman/assmanHandleCCP.hpp
@@ -0,0 +1,43 @@
+
+
+#include "assmanCommons.hpp"
+ 
+// HEADLINE 
+ 
+class titleHandleCCP:titleBarText
+  {
+	text = " SETUP CCP";
+  };
+ 
+
+// BUTTONS
+ 
+ class btn_deployCCP:ButtonControlContentWide 
+  {
+    
+	idc = 40;
+    type = 1;
+	
+    x = 0.4;
+    y = 0.37;
+	
+    sizeEx = 0.054;
+			
+	text = "DEPLOY CCP";
+	action = "[] call gnrf_fnc_deployCCP; closeDialog 0;"	
+  };
+
+   class btn_deconstructCCP:ButtonControlContentWide 
+  {
+    
+	  idc = 41;
+    type = 1;
+	
+    x = 0.4;
+    y = 0.45;
+	
+    sizeEx = 0.054;
+			
+	text = "DECONSTRUCT CCP";
+	action = "[] call gnrf_fnc_deconstructCCP; closeDialog 0;"	
+  };
